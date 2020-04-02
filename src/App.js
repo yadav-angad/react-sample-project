23a5a93ef723826755be2f18572ba07005cbf4e3
JDNET\ay86452 <yadavangad@johndeere.com>
2020-04-01 23:38:26 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
-import "./styles.css";
+import { Route, Switch } from 'react-router-dom';
+import "./App.css";
+import "./style/style.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import User from "./components/User";
+import ErrorHandling from "./exception/ErrorHandling";
+import Error from "./components/Error";
+import userDetails from './components/UserDetail';
 
-export default function App() {
+
+function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <ErrorHandling>
+        <Header />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/user" component={User} exact/>
+          <Route path="/user/:userId" component={userDetails} />
+          <Route component={Error} />
+        </Switch>
+        <Footer />
+      </ErrorHandling>
+    </>
   );
 }
+
+export default App;
